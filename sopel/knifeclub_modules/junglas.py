fa9a7002e2ff6e89039adefabceea1138bf4f397
uname-a <billybob2435@gmail.com>
2015-11-11 15:39:35 -0600

    hoonglass testing

--- sopel/knifeclub_modules/junglas.py
+++ sopel/knifeclub_modules/junglas.py
@@ -4,4 +4,5 @@ from sopel import module
 
 @module.rule('((j+u+)|(h+oo+))n+g+l+a+s+')
 def junglas(bot, trigger): 
-    bot.say('HOOOOOOONGLAAAAASSSSSSSSSSS!!!!!111!!!111')
+    #bot.say('HOOOOOOONGLAAAAASSSSSSSSSSS')
+    bot.say('((j+u+)|(h+oo+))n+g+l+a+s+')
