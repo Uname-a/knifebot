c5518f980d379cef36ea8de6623a563580ec63fb
Adam Cooperman <coopermanster@gmail.com>
2015-11-11 19:47:09 -0500

    stupid quotes test

--- sopel/knifeclub_modules/junglas.py
+++ sopel/knifeclub_modules/junglas.py
@@ -3,5 +3,5 @@
 from sopel import *
 
 @module.rule('.*((j+u+)|(h+oo+))n+g+l+a+s+.*')
-def junglas(bot, trigger): 
-    bot.say('HOOOOOOONGLAAAAASSSSSSSSSSS!!!!!111!!!111 But also Im testing Git right now.....pleasework!!')
+def junglas(jot, trigger): 
+    bot.say("HOOOOOOONGLAAAAASSSSSSSSSSS!!!!!111!!!111 I'm testing quotes :P")
