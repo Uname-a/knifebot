9df60e5a9a5ee1c0fc5bc871d396b41421b2f505
Matt Baird <Uname-a@users.noreply.github.com>
2015-11-11 18:20:43 -0600

    Update junglas.py

--- sopel/knifeclub_modules/junglas.py
+++ sopel/knifeclub_modules/junglas.py
@@ -4,4 +4,4 @@ from sopel import module
 
 @module.rule('((j+u+)|(h+oo+))n+g+l+a+s+')
 def junglas(bot, trigger): 
-    bot.say("HOOOOOOONGLAAAAASSSSSSSSSSS!!!!!111!!!111 But also Im testing Git right now...")
+    bot.say('HOOOOOOONGLAAAAASSSSSSSSSSS!!!!!111!!!111 But also Im testing Git right now...')
