dd83fafef1fa222b613f1044fb67c0288a0a07e0
Adam Cooperman <coopermanster@gmail.com>
2015-11-11 19:57:47 -0500

    I'm a big dumb dumb

--- sopel/knifeclub_modules/junglas.py
+++ sopel/knifeclub_modules/junglas.py
@@ -3,5 +3,5 @@
 from sopel import *
 
 @module.rule('.*((j+u+)|(h+oo+))n+g+l+a+s+.*')
-def junglas(jot, trigger): 
+def junglas(bot, trigger): 
     bot.say('HOOOOOOONGLAAAAASSSSSSSSSSS!!!!!111!!!111 I\'m testing quotes :P')
