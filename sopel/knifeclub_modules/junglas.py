972bde30560eeeaa007618ba10c8e04d9605bc19
Adam Cooperman <coopermanster@gmail.com>
2015-11-11 19:42:33 -0500

    one last try?

--- sopel/knifeclub_modules/junglas.py
+++ sopel/knifeclub_modules/junglas.py
@@ -1,12 +1,7 @@
-
 # coding=utf8
 """Junglas module for Sopel"""
 from sopel import *
 
-@module.rule('((j+u+)|(h+oo+))n+g+l+a+s+')
+@module.rule('.*((j+u+)|(h+oo+))n+g+l+a+s+.*')
 def junglas(bot, trigger): 
-<<<<<<< HEAD
     bot.say('HOOOOOOONGLAAAAASSSSSSSSSSS!!!!!111!!!111 But also Im testing Git right now.....pleasework!!')
-=======
-    bot.say('HOOOOOOONGLAAAAASSSSSSSSSSS')
->>>>>>> 7ec1b9a7e995a1aa396d1218bc8128d575e19c22
